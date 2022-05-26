621898c2e8755f3b2b50fb2cde543c9f06c15d46
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-26 13:37:13 +0500

    Install & configure CSS loader #18

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
    },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
    output: {
      filename: '[name].bundle.js',
        path: path.resolve(__dirname, 'dist'),
      clean: true,
-   },
+     },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+           },
+        {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
  };
